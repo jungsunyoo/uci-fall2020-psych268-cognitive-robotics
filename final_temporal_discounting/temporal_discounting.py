96df18c6317dda618123518ea5fc2375045e6393
yoojungsun0 <yoojungsun0@gmail.com>
2020-12-10 07:26:59 +0900

    Version 1: hunger constant over time

--- final_temporal_discounting/temporal_discounting.py
+++ final_temporal_discounting/temporal_discounting.py
@@ -21,7 +21,7 @@ robot = Supervisor()
 # degrees for left, right, up, down
 head_directions = [90, 270, 0, 180]
 
-desired_direction = 0
+desired_direction = 1
 
 
 # set random seed for reproducibility
@@ -63,19 +63,24 @@ vpre = 0
 trial = 0 
 tLatency = 0 
 found = False
-HD_RES = 1.0
+HD_RES = 5.0#1.0
 TURN_RATE = 100
 GAIN = 2.5
 threshold = 0.5#0.25 #0.4#0.25
-max_speed = 6.28
+max_speed = 2#6.28
 radius = 1
 alpha = 0.5
 time_unit = 10
+
+forward = True
 
 # do this once only
 
 startingTranslation = [[0.7, 0, -4],
                         [-0.7, 0, -4]]
+                        
+# startingTranslation = [[8.15, 0.125, 0.4],
+                        # [8.15, 0.125, 0.4]] # Testing
 
 startingRotation = [0, 1, 0, 3]
 
@@ -93,6 +98,11 @@ rot = node.getField("rotation")
 
 # Functions relevant to task
 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 # Getting distance
 def getDistance(x1, y1, x2, y2): 
     x = (x1-x2)**2
@@ -100,17 +110,31 @@ def getDistance(x1, y1, x2, y2):
     return (x+y)**0.5
     
 # Temporal discounting
+
+
+
+def modulated_impulsivity(kval, hunger):
+    return kval * hunger
+    
+
 def discounted_value(kval, delay, value):
     DV = 1 / (1+ (kval * delay))
     discounted_value = DV * value
     return discounted_value
+    
+
     
 # Reward prediction error
 def RPE(alpha, previous_value, actual_reward):
     current_value = previous_value + alpha * (actual_reward - previous_value)
     return current_value
 
-hunger_levels = [1, 10, 100]
+
+def motivation(value, hunger):
+    effort = sigmoid(value / hunger)
+    return effort
+
+hunger = 1 #[1, 5, 10]
 kval = 0.01 #[0.01, 0.05, 0.1]
 
 # red apple (LL) = 100, green apple (SS) = 50
@@ -124,15 +148,18 @@ LLSS_translations = [LL_translation, SS_translation]
 
 # LL_value = 0
 # SS_value = 0
-LLSS_values = [0,0] # LL, SS integrated
+LLSS_values = [1e-3,1e-3] # LL, SS integrated ; avoid zero?
+
+LL_magnitude = 100
+SS_magnitude = 75
 
 
 rot.setSFRotation(startingRotation)
 if random.random() < 0.5: 
-    trans.setSFVec3f(startingTranslation[0])
+    random_start = 0
 else: 
-    trans.setSFVec3f(startingTranslation[1])
-
+    random_start = 1
+trans.setSFVec3f(startingTranslation[random_start])
 
 LL_table = []
 SS_table = []
@@ -142,9 +169,26 @@ SS_table = []
 while robot.step(timestep) != -1:
     time0 = time.time()
 
-
-
-
+    # calculate motivation to determine baseline speed
+    if trial == 0:
+        effort = motivation(LLSS_values[random_start], hunger)
+    else:
+        effort = motivation(LLSS_values[next], hunger)
+    
+    baseline_speed = effort * max_speed
+    
+    
+    
+    
+    #init speeds
+    left_speed = 1 #1#max_speed * 0.5
+    right_speed = 1#max_speed * 0.5
+    
+    # calculate k-value modulated by current level of hunger
+    mk = modulated_impulsivity(kval, hunger)
+    
+    
+    
     psValues=[]
     for i in range(8):
         psValues.append(ps[i].getValue())  
@@ -157,9 +201,7 @@ while robot.step(timestep) != -1:
     front_obstacle = psValues[2] < threshold
  
     
-    #init speeds
-    left_speed = 1#max_speed * 0.5
-    right_speed = 1#max_speed * 0.5
+
     
     trans_value = trans.getSFVec3f()
     # print(trans_value)
@@ -176,100 +218,111 @@ while robot.step(timestep) != -1:
         ofRight += psValues[i]
         ofLeft += psValues[i+4]
 
+
+
+   # read compass
+    cmpXY = cmpXY1.getValues()
+    cmpZ = cmpZ1.getValues()
+
+    
+    # calculate bearing
+    rad = math.atan2(cmpXY[0], cmpZ[2])
+    bearing = rad / 3.1428 * 180.0
+    if bearing < 0.0: 
+        bearing = bearing + 360.0
+
+    heading_error = head_directions[desired_direction] - bearing
+    if abs(heading_error) < HD_RES: 
+        # print('a')
+        turning = False
+        t=1
+        forward = True
+        # t += 1
+    elif head_directions[desired_direction] == 0 and bearing > 355.0: 
+        # print('b')
+        turning = False
+        forward = True
+        t=1
+        # t += 1
+    elif heading_error > 0: 
+        turning= True
+        forward = False
+        if abs(heading_error) < 180.0:
+            # print('c') 
+            left_speed += 0.5 #* max_speed
+            right_speed -= 0.5 #* max_speed            
+            # left_speed = 3.0
+            # right_speed = -1.0
+        else: 
+            # print('d')
+            left_speed -= 0.5 #* max_speed
+            right_speed += 0.5 #* max_speed 
+            # left_speed = -1.0
+            # right_speed = 3.0
+    else: 
+        turning = True
+        forward = False
+        if abs(heading_error) < 180.0: 
+            # turn left
+            # print('e')
+            left_speed -= 0.5 #* max_speed
+            right_speed += 0.5 #* max_speed                 
+            # left_speed = -1.0
+            # right_speed = 3.0
+        else: 
+            # turn right
+            # print('f')
+            left_speed += 0.5 #* max_speed
+            right_speed -= 0.5 #* max_speed   
+
     # check if the robot hit a wall
     if front_obstacle or left_obstacle or right_obstacle:
+        forward = False
         t=1 
         # modify speeds according to obstacles
         if front_obstacle:
-            print('front_obstacle')
+            # print('front_obstacle')
             left_speed = 0 
             right_speed = -1.0
     
             # print('front obstacle')
     
         elif left_obstacle: 
-            print('left_obstacle')
+            # print('left_obstacle')
             # turn right
-            left_speed += 0.5 * max_speed
-            right_speed -= 0.5 * max_speed
+            left_speed += 0.5 #* max_speed
+            right_speed -= 0.5 #* max_speed
     
         elif right_obstacle: 
-            print('right_obstacle')
-            left_speed -= 0.5 * max_speed
-            right_speed += 0.5 * max_speed
+            # print('right_obstacle')
+            left_speed -= 0.5 #* max_speed
+            right_speed += 0.5 #* max_speed
     elif turning: 
-        print('turning')
-         # t += 1
-        # read compass
-        cmpXY = cmpXY1.getValues()
-        cmpZ = cmpZ1.getValues()
-
-        
-        # calculate bearing
-        rad = math.atan2(cmpXY[0], cmpZ[2])
-        bearing = rad / 3.1428 * 180.0
-        if bearing < 0.0: 
-            bearing = bearing + 360.0
-    
+        # print('turning')
+         t += 1
+     
     
         # pointing close to the desired heading 
-        print(bearing)
-        heading_error = head_directions[desired_direction] - bearing
-        if abs(heading_error) < HD_RES: 
-            turning = False
-            t=1
-            # t += 1
-        elif head_directions[desired_direction] == 0 and bearing > 355.0: 
-            turning = False
-            t=1
-            # t += 1
-        elif heading_error > 0: 
-            if abs(heading_error) < 180.0: 
-                left_speed += 0.5 * max_speed
-                right_speed -= 0.5 * max_speed            
-                # left_speed = 3.0
-                # right_speed = -1.0
-            else: 
-                left_speed -= 0.5 * max_speed
-                right_speed += 0.5 * max_speed 
-                # left_speed = -1.0
-                # right_speed = 3.0
-        else: 
-            if abs(heading_error) < 180.0: 
-                # turn left
-                left_speed -= 0.5 * max_speed
-                right_speed += 0.5 * max_speed                 
-                # left_speed = -1.0
-                # right_speed = 3.0
-            else: 
-                # turn right
-                left_speed += 0.5 * max_speed
-                right_speed -= 0.5 * max_speed   
+        # print(bearing)
     # else: 
     else: 
-        left_speed = 2
-        right_speed = 2
+        # left_speed = 2
+        # right_speed = 2
+        forward = True
         t += 1           
+    
+    
+    if forward:
+        left_speed = baseline_speed
+        right_speed = baseline_speed
+    
 
-    velo = GAIN * ((ofLeft - ofRight)/(ofLeft + ofRight))
-
-    # left_speed += velo * left_speed;
-    # right_speed += -velo*right_speed;
-
-    # if left_speed > max_speed: 
-        # left_speed = max_speed
-    # elif left_speed < 0: 
-        # left_speed = 0 
-    # if right_speed > max_speed: 
-        # right_speed = max_speed
-    # elif right_speed < 0: 
-        # right_speed = 0 
 
 
     leftMotor.setVelocity(left_speed)
     rightMotor.setVelocity(right_speed)
     
-    print(t)
+    # print(t)
     
     
     if found or (not turning and (t % TURN_RATE == 0)):
@@ -279,13 +332,13 @@ while robot.step(timestep) != -1:
     if distance_LL < radius:
         elapsed_time = time.time() - time0
         found = True #def discounted_value(kval, delay, value):
-        TD_value = discounted_value(kval, elapsed_time/time_unit, 100)
+        TD_value = discounted_value(mk, elapsed_time/time_unit, LL_magnitude)
         LLSS_values[0] = RPE(alpha, LLSS_values[0], TD_value)
         LL_table.append(LLSS_values[0])
     elif distance_SS < radius: 
         elapsed_time = time.time() - time0
         found = True
-        TD_value = discounted_value(kval, elapsed_time/time_unit, 50)
+        TD_value = discounted_value(mk, elapsed_time/time_unit, SS_magnitude)
         LLSS_values[1] = RPE(alpha, LLSS_values[1], TD_value)
         SS_table.append(LLSS_values[1])
 
@@ -293,18 +346,21 @@ while robot.step(timestep) != -1:
     # Valuation: decide whether to go for LL or SS
     if LLSS_values[0] > LLSS_values[1]: 
         # go for LL for most of the time
+        print("Choose LL: value of {}".format(LLSS_values[0]))
         if random.random() < 0.7:
             next = 0
         else:
             next = 1
     elif LLSS_values[0] < LLSS_values[1]: 
         # go for SS for most of the time
+        print("Choose SS: value of {}".format(LLSS_values[1]))
         if random.random() < 0.7:
             next = 1
         else: 
             next = 0
         
     else: # if two values are equivalent
+        print("Equal value of {}".format(LLSS_values[0]))
         if random.random() < 0.5: 
             next = 0
             # go for LL
